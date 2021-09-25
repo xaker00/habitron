42219cea3e3df3d071046a706a219683d1b50a02
Victor Lam <mingmanhk@gmail.com>
2021-09-25 10:56:32 -0700

    change on dasboard

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,19 +1,28 @@
 const router = require("express").Router();
-const { Post, Comment, User } = require("../models");
-const {withAuth} = require("../utils/auth");
+const { Habit, Log, User } = require("../models");
+const { withAuth } = require("../utils/auth");
 
 //show all the habit
 router.get("/", withAuth, async (req, res) => {
   console.log("Start Dashboard Route to get all habit by users");
   try {
-    console.log("getting user habit: ", req.session.user_id);
+    console.log("getting user habit: ", req.session.userId);
     const habitData = await Habit.findAll({
-      
+      where: {
+        user_id: req.session.userId,
+      },
+      attributes: ["id", "category", "description", "status", "created_at"],
+      include: [
+        
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
     });
     // Serialize data so the template can read it
-    console.log(habitData);
     const habits = habitData.map((habit) => habit.get({ plain: true }));
-    console.log(habit);
+    console.log(habits);
     // Pass serialized data and session flag into template
     res.render("dashboard", {
       habits,
