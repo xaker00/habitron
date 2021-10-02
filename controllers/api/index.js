664827b70a15dad37b15f2b4501b05755ba667cc
Victor Lam <mingmanhk@gmail.com>
2021-10-01 18:39:38 -0700

    Add feature to add habit on app page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const habitRoutes = require('./habitRoutes');
+const habitRoutes = require("./habitRoutes");
 const chartRoutes = require("./chartRoutes");
 const logRoutes = require('./logRoutes');
 
 router.use('/users', userRoutes);
-router.use('/habits', habitRoutes);
+router.use("/habits", habitRoutes);
 router.use('/log', logRoutes);
 router.use("/chart", chartRoutes);
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,35 @@
+
+
 const newHabitHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the login form
-    const habit = document.querySelector(".new-habit").value.trim();
-    console.log(habit);
-    
-    const response = await fetch("/api/habitRoutes", {
-        method: "POST",
-        body: JSON.stringify( { habit } ),
-        headers: { "Content-Type": "application/json" },
-      });
+  event.preventDefault();
+
+  // Collect values from the login form
+  const habit = document.querySelector(".new-habit").value.trim();
+  console.log(habit);
+
+  if (habit) {
+    const response = await fetch("/api/habits", {
+      method: "POST",
+      body: JSON.stringify({ habit }),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(response);
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
 
+// document
+//   .querySelector(".habit-form")
+//   .addEventListener("keypress", function (e) {
+//     if (e.key === "Enter") {
+//       newHabitHandler;
+//     }
+//   });
 
-
-
-
-
-
-document
-  .querySelector('.habit-form')
-  .addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {newHabitHandler}
-    });
-
+  document
+    .querySelector(".btn-post-add")
+    .addEventListener("click", newHabitHandler);
